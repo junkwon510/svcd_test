08d272507fcaaca2ec33ac5f98d25d9ad2ad9412
junkwon510 <165608285+junkwon510@users.noreply.github.com>
2024-04-25 16:17:03 +0900

    Update prac.py

--- prac.py
+++ prac.py
@@ -116,10 +116,17 @@ with tab_1:
         
         with st.expander(i, expanded=True): # i: AAE325A, AAE333A, ...
             minv, maxv = st.columns(2)
-            with minv:
-                min_val = st.number_input(f"Minimum {i}", min_value=0.0, max_value=160.0, value=0.0, step=0.01, key=f"{i}_min")
-            with maxv:
-                max_val = st.number_input(f"Maximum {i}", min_value=0.0, max_value=160.0, value=160.0, step=0.01, key=f"{i}_max")
+            if i[2]== 'D':
+                with minv:
+                    min_val = st.number_input(f"Minimum {i}", min_value=0.0, max_value=160.0, value=0.0, step=0.01, key=f"{i}_min")
+                with maxv:
+                    max_val = st.number_input(f"Maximum {i}", min_value=0.0, max_value=160.0, value=160.0, step=0.01, key=f"{i}_max")
+            else:
+                with minv:
+                    min_val = st.number_input(f"Minimum {i}", min_value=0.0, max_value=120.0, value=0.0, step=0.01, key=f"{i}_min")
+                with maxv:
+                    max_val = st.number_input(f"Maximum {i}", min_value=0.0, max_value=120.0, value=120.0, step=0.01, key=f"{i}_max")
+                    
         st.markdown("###") # 사이사이 공간 만들어주는 용도
 
         # Range Setting에서 min, max 같을 때 메시지 띄워주기
